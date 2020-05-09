3bd2023a255f41a2b75d16cec0b6e11e54354573
missteelee <64831093+missteelee@users.noreply.github.com>
2020-05-09 14:51:09 -0700

    Add files via upload

---
+++ footer.inc.html.php
@@ -0,0 +1,3 @@
+<footer>
+    <p>Copyright &copy; <?php echo date("Y"); ?> Ace in the Hole Multisport Events<br><a href="mailto:aceinthehole@multisportevents.com">aceinthehole@multisportevents.com
+</footer>
---
+++ header.inc.html.php
@@ -0,0 +1,7 @@
+<?php define('BASE_URL', 'cas222\AITH\template'); ?>
+
+<header class="w3-container w3-top w3-hide-large w3-red w3-xlarge w3-padding">
+  <a href="javascript:void(0)" class="w3-button w3-red w3-margin-right" onclick="w3_open()">
+<img src="<?php echo BASE_URL; ?>title.png" alt="Ace in the Hole Multisport Events">
+  
+</header>
