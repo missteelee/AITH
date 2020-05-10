5501e3954f133befaea97dbe753b4c98962d8e57
missteelee <64831093+missteelee@users.noreply.github.com>
2020-05-09 17:39:41 -0700

    Rename template.php to template1.php

--- footer.php
+++
@@ -1,4 +0,0 @@
-<footer>
-    <p>Copyright &copy; <?php echo date("Y"); ?> Ace in the Hole Multisport Events<br>
-        <a href="mailto:aceinthehole@multisportevents.com">aceinthehole@multisportevents.com</a></p>
-</footer>
