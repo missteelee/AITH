a6df6377cc50f820d80714c24785d177cd9cab45
missteelee <64831093+missteelee@users.noreply.github.com>
2020-06-02 21:06:32 -0700

    registration

--- index.php
+++ index.php
@@ -1,104 +1,63 @@
 <?php
-/**
- * This example shows how to handle a simple contact form.
- */
- 
- include  '../includes/db.inc.html.php';
+// Edit or Replace this try/catch statement to work with the current PHT configuration
 
-$msg = '';
+include  '../includes/db.inc.html.php';
 
-//Don't run this unless we're handling a form submission
-if (isset($_POST['myname'])) {
-    date_default_timezone_set('Etc/UTC');
-
-    $myname = $_POST['myname'];
-    $myemail = $_POST['myemail'];
-    $myrole = $_POST['myrole'];
-    $myquestion = $_POST['myquestion'];
-
+        // Modify the If statement so the try only runs if the First Name field has been submitted AND the honeypot field is empty ''
+if (isset($_POST['full_name']))
+  {
+      $full_name = $_POST['full_name'];
+      $age = $_POST['age'];
+      $role = $_POST['role'];
+      $email = $_POST['email'];
+      $emergency_contact = $_POST['emergency_contact'];
+      $emergency_phone = $_POST['emergency_phone'];
+      $gender = $_POST['gender'];
+      $saturday_courses = $_POST['saturday_courses'];
+      $sunday_courses = $_POST['sunday_courses'];
+      $special_accommodations = $_POST['special_accommodations'];
     try {
-        $sql = 'INSERT INTO contact SET
-        myname = :myname,
-        myemail = :myemail,
-        myrole = :myrole,
-        myquestion = :myquestion';
-        
-        $s = $pdo->prepare($sql);
-        $s->bindValue(':myname', $myname);
-        $s->bindValue(':myemail', $myemail);
-        $s->bindValue(':myrole', $myrole);
-        $s->bindValue(':myquestion', $myquestion);
-        $s->execute();
+      $sql = 'INSERT INTO registration SET
+      
+        full_name = :full_name,
+        age = :age,
+        role = :role,
+        email = :email,
+        emergency_contact = :emergency_contact,
+        emergency_phone = :emergency_phone,
+        gender = :gender,
+        saturday_courses = :saturday_courses,
+        sunday_courses = :sunday_courses,
+        special_accommodations = :special_accommodations';
+
+      $s = $pdo->prepare($sql);
+      $s->bindValue(':full_name', $full_name);
+      $s->bindValue(':age', $age);
+      $s->bindValue(':role', $role);
+      $s->bindValue(':email', $email);
+      $s->bindValue(':emergency_contact', $emergency_contact);
+      $s->bindValue(':emergency_phone', $emergency_phone);
+      $s->bindValue(':gender', $gender);
+        $s->bindValue(':saturday_courses', $saturday_courses);
+        $s->bindValue(':sunday_courses', $sunday_courses);
+      $s->bindValue(':special_accommodations', $special_accommodations);
+      $s->execute();
     }
-    
     catch (PDOException $e)
     {
-      $error = 'Error adding submitted registration: ' . $e->getMessage();
+      $error = 'Error adding submitted registration form: ' . $e->getMessage();
       include '../includes/error.html.php';
       exit();
     }
+    // load the thank you page after the INSERT runs
+      include 'success.html.php';
+    // Add an else to load the initial page if the initial (line 19) if statement is false
     
-    require '../PHPMailer/PHPMailerAutoload.php';
-
-    //Create a new PHPMailer instance
-    $mail = new PHPMailer;
-    //Tell PHPMailer to use SMTP - requires a local mail server
-    //Faster and safer than using mail()
-    $mail->isSMTP();
-    $mail->Host = 'mail.tracilee.webhostingforstudents.com';
-    $mail->Port = 465;
-    //Set this to true if SMTP host requires authentication to send email
-    $mail->SMTPAuth = true;
-    //Provide username and password
-    $mail->Username = "phpmailer@tracilee.webhostingforstudents.com";
-    $mail->Password = "G03258374Traci";
-
-    //Use a fixed address in your own domain as the from address
-    //**DO NOT** use the submitter's address here as it will be forgery
-    //and will cause your messages to fail SPF checks
-    $mail->setFrom('phpmailer@tracilee.webhostingforstudents.com', 'Traci Lee');
-    //Send the message to yourself, or whoever should receive contact for submissions
-    $mail->addAddress('traci.lee@pcc.edu', 'Traci Lee');
-    //Put the submitter's address in a reply-to header
-    //This will fail if the address provided is invalid,
-    //in which case we should ignore the whole request
-    $mail->addReplyTo($myEmail, $myName);
-        $mail->Subject = 'AITH Contact Form';
-        //Keep it simple - don't use HTML
-        $mail->isHTML(true);
-        //Build a simple message body
-        $mail->Body = <<<EOT
-Contact Name: $myname<br>
-Contact Email: $myemail<br>
-Whether the person is an athlete, volunteer or interested party: $myrole<br>
-Question or Comment: $myquestion
-EOT;
-
-    //Send the message, check for errors
-    if (!$mail->send()) {
-            //The reason for failing to send will be in $mail->ErrorInfo
-            //but you shouldn't display errors to users - process the error, log it on your server.
-            echo "Mailer Error:" .  $mail->ErrorInfo;
-    } else {
-            include 'success.html.php';
-    }
-        
-//honey pot field
-	$honeypot = $_POST['myname'];
-
-	//check if the honeypot field is filled out. If not, send a mail.
-	if( !empty( $honeypot ) ){
-	    return;
-	}
-	else{
-		include '../includes/error.html.php';
-	}
-	
-    
+  }
+		
+else {
+  include 'registration.html.php';
 }
-    
-    else {
-        include 'contact.html.php';
-    }
-    	
+
 ?>
+
