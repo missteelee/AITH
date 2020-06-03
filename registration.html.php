8a78436d523fc2c0f6331871b182390de7cc5f1f
missteelee <64831093+missteelee@users.noreply.github.com>
2020-06-03 01:41:14 -0700

    Update registration.html.php

--- registration.html.php
+++ registration.html.php
@@ -2,17 +2,21 @@
 
 <html lang="en">
 <head>
-<title>Ace in the Hole Multisport Events</title>
+<title>Contact Form</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="author" content="Traci Lee">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">  
-    <link href="../styles/style.less" rel="stylesheet/less" type="text/css">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins"> 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.css" >
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <link rel="stylesheet" href="form.css" >
+  <link href="../styles/style.less" rel="stylesheet/less" type="text/css">
   <link rel="stylesheet" href=../styles/style.css type="text/css">  
-</head>
-
-<body>
+  <script src="form.js"></script>
+    </head>
+ <body>
+  
 <!-- Sidebar/menu -->
 <nav class="w3-sidebar w3-red w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:300px;font-weight:bold;" id="mySidebar"><br>
   <a href="javascript:void(0)" onclick="w3_close()" class="w3-button w3-hide-large w3-display-topleft" style="width:100%;font-size:22px">Close Menu</a>
@@ -20,11 +24,11 @@
 <section class="w3-container">
     <h3><img src="../images/logo.jpg" alt="Site Logo" style="width:50%;"></h3>
   <div class="w3-bar-block">
-    <a href="../template/template.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Home</a>
-    <a href="../template/template.php#course_details" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Course Details</a>
-    <a href="registration.html.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Registration</a>
-    <a href="../template/template.php#faqs" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">FAQs</a>
-    <a href="../template/template.php#contact" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Contact Us</a>
+    <a href="../template/template.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Home</a> 
+    <a href="../template/template.php#course_details" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Course Details</a> 
+    <a href="#" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Registration</a> 
+    <a href="../template/template.php#faqs" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">FAQs</a> 
+    <a href="../contact/contact.html.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Contact Us</a>
   </div>
   </section>
 </nav>
@@ -71,14 +75,14 @@
   <div class="w3-container" id="register" style="margin-top:75px">
     <h1>Registration</h1>
     <hr>
-<form id="registrationform" method="POST" action="success.html.php">
+     <form method="post" id="reused_form" >
       <div class="w3-section">
         <label>Full Name</label>
         <input class="w3-input w3-border" type="text" name="full_name" id="full_name" required>
       </div>
         <div class="w3-section">
         <label>Age</label>
-        <input class="w3-input w3-border" type="number" name="age" id="age" required>
+        <input class="w3-input w3-border" type="number" name="age" id="age">
       </div>    
       <div class="w3-section">
           <label for="role">You have registered as</label>
@@ -134,11 +138,15 @@
         <input class="w3-input w3-border" type="text" name="special_accommodations" required>
       </div>    
   
-        <input id="mysubmit" type="submit" class="w3-button w3-block w3-padding-large w3-red w3-margin-bottom" value="Submit"> 
+        <input id="mysubmit" type="submit" class="w3-button w3-block w3-padding-xlarge w3-red w3-margin-bottom" value="Submit"> 
     </form>  
-  </div>
-</section>
-    </main>   
+    
+ <div id="success_message" style="display:none">
+                    <h3>Submitted the form successfully!</h3> 
+                    <p> We will get back to you soon. </p>
+                </div>
+                <div id="error_message" style="width:100%; height:100%; display:none; "> <h3>Error</h3> Sorry there was an error sending your form. </div>
+            </div> 
     
 <div class="w3-light-grey w3-container w3-padding-32" style="margin-top:75px;padding-right:58px"><p class="w3-right">
   
