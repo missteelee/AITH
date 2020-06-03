8482ed49cef63c4a48bdef316a7785604a9a178b
missteelee <64831093+missteelee@users.noreply.github.com>
2020-06-02 20:58:37 -0700

    Delete footer.inc.html.php

--- footer.inc.html.php
+++
@@ -1,3 +0,0 @@
-<footer>
-    <p style="text-align: center; margin-left: 50px; ">Copyright &copy; <?php echo date("Y"); ?> Ace in the Hole Multisport Events<br><a href="mailto:aceinthehole@multisportevents.com">aceinthehole@multisportevents.com
-</footer>
