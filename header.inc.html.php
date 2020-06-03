3eca6e2e71796304c22a8a7ad8c38f6cf7da0cbf
missteelee <64831093+missteelee@users.noreply.github.com>
2020-06-02 21:00:37 -0700

    Add files via upload

---
+++ header.inc.html.php
@@ -0,0 +1,18 @@
+
+  <?php define('BASE_URL', '/cas222/AITH/contact'); ?>
+
+<header class="w3-container w3-top w3-hide-large w3-red w3-xlarge w3-padding">
+  <a href="javascript:void(0)" class="w3-button w3-red w3-margin-right w3-padding" onclick="w3_open()">☰</a>
+  <span><img src="logo.jpg" alt="Site Logo" style="width:5%;"></span>
+</header>
+  
+<!-- Overlay effect when opening sidebar on small screens -->
+<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+  <!-- Header -->
+<div class="w3-main" style="margin-left:340px;margin-right:40px">
+<div class="w3-container" style="margin-top: 60px" id="showcase">
+    <h1 class="w3-jumbo"><b>Ace in the Hole</b></h1>
+    <h1 class="w3-xxxlarge w3-text-black"><b>Multisport Events</b></h1>
+    <hr style="width:50px;border:5px solid red" class="w3-round">
+  </div>
